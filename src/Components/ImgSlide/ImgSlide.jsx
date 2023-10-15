6546da30bdaae8d5533627dc88df99a9e63ed96f
anamarincic <amarincic5@gmail.com>
2023-10-15 16:50:28 +0200

    fixed arrows

--- src/Components/ImgSlide/ImgSlide.jsx
+++ src/Components/ImgSlide/ImgSlide.jsx
@@ -3,8 +3,13 @@ import { Slide } from "react-slideshow-image";
 import "react-slideshow-image/dist/styles.css";
 
 export function ImgSlide() {
+  const properties = {
+    prevArrow: <button className="buttonStyle">&lt;</button>,
+    nextArrow: <button className="buttonStyle">&gt;</button>,
+  };
+
   return (
-    <Slide>
+    <Slide {...properties}>
       <div className="each-slide-effect">
         <div className="slideOne">
           <span>DISCIPLINA</span>
