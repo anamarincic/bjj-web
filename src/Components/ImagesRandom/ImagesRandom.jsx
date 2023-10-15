4ef20e77a56d1adf7b740b742b93e0487cefab08
anamarincic <amarincic5@gmail.com>
2023-10-15 15:45:50 +0200

    fixed bg-img

--- src/Components/ImagesRandom/ImagesRandom.jsx
+++ src/Components/ImagesRandom/ImagesRandom.jsx
@@ -6,37 +6,37 @@ export function ImagesRandom() {
       <div className="images-random__content">
         <img
           src={require("../../images/random/randomOne.jpg")}
-          alt="jj kimono"
+          alt="jj universum Osijek kimono"
         />
       </div>
       <div className="images-random__content wide">
         <img
           src={require("../../images/random/randomTwo.jpg")}
-          alt="jj universum kimono "
+          alt="jj universum kimono Osijek "
         />
       </div>
       <div className="images-random__content">
         <img
           src={require("../../images/random/randomThree.jpg")}
-          alt="plavi kimono"
+          alt="plavi kimono Osijek"
         />
       </div>
       <div className="images-random__content wide">
         <img
           src={require("../../images/random/randomFour.jpg")}
-          alt="borilačka vještina za djeca "
+          alt="borilačka vještina za djeca universum Osijek"
         />
       </div>
       <div className="images-random__content">
         <img
           src={require("../../images/random/randomFive.jpg")}
-          alt="bjj kimono"
+          alt="bjj kimono Osijek"
         />
       </div>
       <div className="images-random__content ">
         <img
           src={require("../../images/random/randomSix.jpg")}
-          alt="jj izlet djece "
+          alt="jj universum Osijek izlet djece "
         />
       </div>
     </div>
